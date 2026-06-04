82c76db64bdf57ce17b4de4ce1535cc140717dda
erick <erickndeto202@gmail.com>
2026-06-04 12:16:32 +0300

    Fix formatting of database credentials string

--- env.go
+++ env.go
@@ -16,11 +16,11 @@ func Config()(string, error){
 		return envERR
 	}//postgres://username:password@localhost:5432/dbname?sslmode=disable
 	dbCreds := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
-        os.Getenv("DB_USER"),
-	os.Getenv("DB_PASSWORD"),
-	os.Getenv("DB_HOST"),
-	os.Getenv("DB_PORT"),
-	os.Getenv("DB_NAME"),
+						   os.Getenv("DB_USER"),
+						   os.Getenv("DB_PASSWORD"),
+						   os.Getenv("DB_HOST"),
+						   os.Getenv("DB_PORT"),
+						   os.Getenv("DB_NAME")
 )
 return dbCreds, nil
 }
